a067a8a093ca254b4a86ef14b8a032071df770e6
Roger Chapman <rogchap@gmail.com>
2020-10-22 13:57:20 +1100

    initial setup of project

---
+++ frontend/src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+	import Modal from 'svelte-simple-modal';
+	import HelloWorld from './components/HelloWorld.svelte'
+	import logo from './logo.png';
+</script>
+
+<main>
+	<div class="App">
+		
+	</div>
+</main>
+
+<style>
+    :root {
+       --bg-color: #2e3440;
+       --bg-color2: #434c5e;
+    }
+
+	:global(body) {
+		margin: 0;
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+			"Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+			sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+        background-color: var(--bg-color);
+	}
+
+</style>
+
+
+    <!-- readonly property color bgColor: "#2e3440" -->
+    <!-- readonly property color bgColor2: "#434c5e" -->
+    <!-- readonly property color bgColor3: "#4c566a" -->
+    <!-- readonly property color bgLightColor: "#eceff4" -->
+    <!-- readonly property color bgInputColor: "#242933" -->
+    <!--  -->
+    <!-- readonly property color borderColor: "#3b4252" -->
+    <!--  -->
+    <!-- readonly property color textColor: "#eceff4" -->
+    <!-- readonly property color textColor2: "#d8dee9" -->
+    <!-- readonly property color textColor3: "#bcc7d9" -->
+    <!-- readonly property color textDarkColor: "#2e3440" -->
+    <!--  -->
+    <!-- readonly property color primaryColor: "#88c0d0" -->
+    <!-- readonly property color accentColor: "#8fbcbb" -->
+    <!-- readonly property color accentColor2: "#81a1c1" -->
+    <!-- readonly property color accentColor3: "#5e81ac" -->
+    <!--  -->
+    <!-- readonly property color redColor: "#bf616a" -->
+    <!-- readonly property color orangeColor: "#d08770" -->
+    <!-- readonly property color yellowColor: "#ebcb8b" -->
+    <!-- readonly property color greenColor: "#a3be8c" -->
+    <!-- readonly property color purpleColor: "#b48ead" -->
+
---
+++ go.mod
@@ -0,0 +1,8 @@
+module wombat
+
+go 1.15
+
+require (
+	github.com/leaanthony/mewn v0.10.7
+	github.com/wailsapp/wails v1.8.0
+)
