6805ad003a1cb0b9556bc06142264793f5a27974
Roger Chapman <rogchap@gmail.com>
2020-11-01 23:33:23 +1100

    syntax highlighting

--- frontend/src/views/Response.svelte
+++ frontend/src/views/Response.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Prism from "svelte-prismjs";
   export let resp = "";
 </script>
 
@@ -7,13 +8,28 @@
     padding: var(--padding);
     height: calc(100% - 106px);
     overflow: scroll;
-  }
-  pre {
     user-select: text;
     -webkit-user-select: text;
+    color: var(--accent-color2);
+  }
+  .response :global(code::selection, span::selection) {
+    background-color: var(--accent-color2);
+    color: var(--text-color);
+  }
+  .response :global(.punctuation) {
+    color: var(--primary-color);
+  }
+  .response :global(.string) {
+    color: var(--green-color);
+  }
+  .response :global(.number) {
+    color: var(--purple-color);
+  }
+  .response :global(.boolean) {
+    color: var(--accent-color);
   }
 </style>
 
 <div class="response">
-  <pre>{resp}</pre>
+  <Prism language="clike">{resp}</Prism>
 </div>
